b6a503caae43644880acbcd79b7d3eff701d5119
clbokea <clbo@kea.dk>
2019-02-26 09:05:54 +0100

    sjdfgjdsgfh

--- code_from_today/cat.py
+++ code_from_today/cat.py
@@ -2,14 +2,7 @@ import sys
 
 
 def cat(filename):
-    f = open(filename, 'r')
-    text = f.read()
-    print(text)
-    
-    #lines = f.readlines()
-    #print(lines)
-    #for line in f:
-    #    print(line, end='')
+    pass
 
 
 def main():
